3766b8ed2a082de98811a4b605b9c9dbb833cf60
Tyrone2333 <424021424@qq.com>
2017-08-24 10:31:37 +0800

    add gamepad support

--- game.js
+++ game.js
@@ -42,7 +42,6 @@ function writeFree(canvasEle) {
         return false
     }
 }
-
 //  图像旋转：基础变换法
 function rotateImg(img,x,y,width,height,angle) {    //  图像旋转：基础变换法
     ctx.save()
@@ -104,35 +103,56 @@ class Tank{
 class Hero extends Tank{
     constructor (x,y,imgUrl){
         super(x,y,imgUrl)
+        this.isUp = false
+        this.isDown = false
+        this.isLeft = false
+        this.isRight = false
+        this.moveTimer = 0
+        this.gp = null
     }
 
+    move(){
+
+        this.moveTimer = setInterval(function () {
+            if (this.x <= 0){ //  不能出墙
+                this.x = 0
+            }else if (this.x >= 1100){
+                this.x = 1100
+            }
+            if (this.y >= 400){
+                this.y = 400
+            }else if (this.y <= 0){
+                this.y = 0
+            }
+
+            if (this.isUp){
+                this.moveUp()
+            }else if (this.isDown){
+                this.moveDown()
+            }else if (this.isLeft){
+                this.moveLeft()
+            }else if (this.isRight){
+                this.moveRight()
+            }
+        }.bind(this),32)
+    }
     listenTankMove(){
         // 监听tank的移动和发射事件
         // let _this = this
         window.addEventListener("keydown",function (ev) {
             if (this.life != 0){
-                if (this.x <= 0){ //  不能出墙
-                    this.x = 0
-                }else if (this.x >= 1100){
-                    this.x = 1100
-                }
-                if (this.y >= 400){
-                    this.y = 400
-                }else if (this.y <= 0){
-                    this.y = 0
-                }
                     switch (ev.key) {
                         case ",":
-                            this.moveUp()
+                            this.isUp = true
                             break
                         case "o":
-                            this.moveDown()
+                            this.isDown = true
                             break
                         case "a":
-                            this.moveLeft()
+                            this.isLeft = true
                             break
                         case "e":
-                            this.moveRight()
+                            this.isRight = true
                             break
                         case "h":
                             this.fire()
@@ -142,7 +162,79 @@ class Hero extends Tank{
             }
 
         }.bind(this),false)
+        window.addEventListener("keyup",function (ev) {
+            switch (ev.key) {
+                case ",":
+                    this.isUp = false
+                    break
+                case "o":
+                    this.isDown = false
+                    break
+                case "a":
+                    this.isLeft = false
+                    break
+                case "e":
+                    this.isRight = false
+                    break
+            }
+        }.bind(this),false)
+        // 检测手柄连接
+        window.addEventListener("gamepadconnected", function (e) {
+            this.gp = navigator.getGamepads()[e.gamepad.index];
+            /**
+             * this.gp           即是一个 Gamepad 对象
+             * this.gp.mapping   返回空字符串表示没有映射是用这个手柄
+             *              返回 standard 游戏手柄的控制已被映射到标准键盘布局。目前唯一已知的映射是“standard”
+             * axes         摇杆，[-1.0 .. 1.0]
+             */
+//                log(this.gp)
+            console.log("Gamepad 已连接 at index %d: %s. %d buttons, %d axes, %s 映射, %s  timestamp",
+                this.gp.index, this.gp.id,
+                this.gp.buttons.length, this.gp.axes.length,this.gp.mapping,this.gp.timestamp);
+
+        }.bind(this),false);
+        window.addEventListener("gamepaddisconnected", function(e) {
+            console.error("Gamepad 已断开连接 from index %d: %s",
+                e.gamepad.index, e.gamepad.id);
+        }.bind(this),false);
     }
+    isButtonPressed(b) {
+        if (typeof(b) == "object") {
+            return b.pressed;
+        }
+        return b == 1.0;
+    }
+    getControlCommend() {
+    if (this.gp){
+        //                    log(this.gp.axes[0].toFixed(2))
+//            左边摇杆
+        // this.gp.axes[0] 1 => 右方向， -1 => 左方向
+        // this.gp.axes[1] 1 => 下方向
+
+//            右边摇杆
+        // this.gp.axes[2] 1 => 右方向
+        // this.gp.axes[3] 1 => 下方向
+
+        //  this.gp.axes[0] 是形如 0.07541123691518906 极长的数字 .toFixed(2) 保留到两位
+        if (this.gp.axes[0].toFixed(2) >= 0.3){
+            this.moveRight()
+
+        }else if (this.gp.axes[0].toFixed(2) <= -0.3){
+            this.moveLeft()
+
+        }else if (this.gp.axes[1].toFixed(2) >= 0.3){
+            this.moveDown()
+
+        }else if (this.gp.axes[1].toFixed(2) <= -0.3){
+            this.moveUp()
+        }
+
+        if (this.isButtonPressed(this.gp.buttons[0])){
+            this.fire()
+        }
+    }
+}
+
 
 }
 class EnemyTank extends Tank{
@@ -260,6 +352,7 @@ class Game{
     _main(){
 
         this.hero.listenTankMove()
+        this.hero.move()
 
         //  刷新画布，比较消耗性能
         this.timer = setInterval(function () {
@@ -289,8 +382,8 @@ class Game{
 
     refreshCanvas (){
         //  数据刷新和画布的刷新分离
+        this.hero.getControlCommend()
         this.c.clearRect(0,0,this.canvas1.clientWidth,this.canvas1.clientHeight)
-
 
         // 画敌方tank
 
@@ -328,7 +421,6 @@ class Game{
                     this.drawBoom(this.hero)
 
                     this.enemys.splice(i,1)
-                    log(this.enemys)
                     this.hero.life = 0
                     window.clearInterval(this.timer)
                     // log(this.enemys)
@@ -362,7 +454,6 @@ class Game{
                             this.enemys[j]._die()
                             this.hero.score ++
                             this.createNewEnemy()
-                            log(this.enemys)
                             // this.enemys.splice(j,1)  // 会导致敌人死亡时子弹也消失
                         }
                     }
@@ -488,6 +579,7 @@ class Game{
         let bulletState = document.getElementById("bulletState")
         let tankState = document.getElementById("tankState")
         let scoreInfo = document.getElementById("score")
+        let controlInfo = document.getElementById("controlInfo")
 
         tankState.innerHTML = "x: " + this.hero.x + "y: " + this.hero.y
         // log(this.hero.bullets[0])
@@ -496,6 +588,11 @@ class Game{
         }
 
         scoreInfo.innerHTML = this.hero.score
+
+        if (!!this.hero.gp){
+            controlInfo.innerHTML = "&nbsp;&nbsp;手柄已连接在："+this.hero.gp.index+"端口<br>&nbsp;&nbsp;手柄是：" + this.hero.gp.id
+        }
+
 
     }
     rotateImg(ctx,img,x,y,width,height,angle) {    //  图像旋转：基础变换法
@@ -513,5 +610,7 @@ window.onload = function () {
 
     let game = new Game()
     game._main()
+
+
 
 }
